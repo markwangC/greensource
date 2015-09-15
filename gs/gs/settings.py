4d7c1d29c692e0b18f27fba1d90791f43bbd28c0
Mark <mark.c.wang@ericsson.com>
2015-09-15 10:51:44 +0800

    gs

--- gs/gs/settings.py
+++ gs/gs/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for site_warehouse project.
+Django settings for gs project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4arr%^fdwmy9bx2604^)#u3_45)fi&-of@)j%scd=l@+++i2lq'
+SECRET_KEY = 'fox!xr+(!pb-v1qg6*jookm!ds=nsrbs=)drhsk)=g(gg7tvgc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,19 +30,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'warehouse',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',  
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -51,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'site_warehouse.urls'
+ROOT_URLCONF = 'gs.urls'
 
-WSGI_APPLICATION = 'site_warehouse.wsgi.application'
+WSGI_APPLICATION = 'gs.wsgi.application'
 
 
 # Database
@@ -66,24 +63,21 @@ DATABASES = {
     }
 }
 
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'zh_CN'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = BASE_DIR
+
 STATIC_URL = '/static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
--- gs/gs/wsgi.py
+++ gs/gs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for site_warehouse project.
+WSGI config for gs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_warehouse.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gs.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
